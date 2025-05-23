dcaed81e44f7e44d1262d266304eafd75e88fca7
M2K3K5 <seb.main@protonmail.com>
2025-05-23 20:33:53 +0200

    fix: update logger to use local string format for timestamps

--- apps/server/src/tools/logger.ts
+++ apps/server/src/tools/logger.ts
@@ -15,14 +15,14 @@ export function LogLevelAccepts(wantedLogLevel: LogLevel) {
 export const logger = {
   debug: (...args: any) =>
     LogLevelToNumber[logLevel] <= 0 &&
-    console.log(`[debug] [${new Date().toISOString()}]`, ...args),
+    console.log(`[debug] [${new Date().toLocaleString()}]`, ...args),
   info: (...args: any) =>
     LogLevelToNumber[logLevel] <= 1 &&
-    console.log(`[info] [${new Date().toISOString()}]`, ...args),
+    console.log(`[info] [${new Date().toLocaleString()}]`, ...args),
   warn: (...args: any) =>
     LogLevelToNumber[logLevel] <= 2 &&
-    console.warn(`[warn] [${new Date().toISOString()}]`, ...args),
+    console.warn(`[warn] [${new Date().toLocaleString()}]`, ...args),
   error: (...args: any) =>
     LogLevelToNumber[logLevel] <= 3 &&
-    console.error(`[error] [${new Date().toISOString()}]`, ...args),
+    console.error(`[error] [${new Date().toLocaleString()}]`, ...args),
 };
